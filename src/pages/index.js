ea8e750b3ec8bf9f22cc5962cd50efc4a41e7a1b
Sanojan <sanojan99@gmail.com>
2023-05-02 11:21:45 +1000

    updated hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import Projects from '../components/Projects';
-import ParticlesBg from '../components/ParticlesBg';
-import favicon from '../images/favicon3.ico';
-import { ToastContainer } from 'react-toastify';
-import '../styles/global.css';
-import Footer from '../components/Footer';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Projects from "../components/Projects";
+import ParticlesBg from "../components/ParticlesBg";
+import favicon from "../images/favicon3.ico";
+import { ToastContainer } from "react-toastify";
+import "../styles/global.css";
+import Footer from "../components/Footer";
+import { Helmet } from "react-helmet";
 
 // markup
 const IndexPage = () => {
@@ -18,7 +18,7 @@ const IndexPage = () => {
         <link rel="icon" href={favicon} />
         <meta
           name="description"
-          content="Sanojan Thiyagaraja | Software Engineering Student at UNSW"
+          content="Sanojan Thiyagaraja | UNSW Computer Science Graduate"
         />
       </Helmet>
       <Header />
